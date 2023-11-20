ea307142250ca7d99c388f1d57437d5a698a1a06
Mario Cares <mario.cares.c@outlook.com>
2023-11-20 12:26:05 -0300

    Fix Type on api/users/[role]/GET

--- app/api/users/[role]/route.ts
+++ app/api/users/[role]/route.ts
@@ -1,11 +1,10 @@
-import { NextApiRequest } from "next";
 import { User, $Enums } from "@prisma/client";
 import Role = $Enums.Role;
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 export async function GET(
-  request: NextApiRequest,
+  request: NextRequest,
   { params }: { params: { role: string } },
 ): Promise<NextResponse<User[]>> {
   const role: Role = params.role as Role;
